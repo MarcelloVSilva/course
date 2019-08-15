4a0181bf7ee505abd5fb10c3d62268a811bb12e3
Marcello Silva <marcellovcs@gmail.com>
2019-08-15 17:44:54 -0300

    double click to solve

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ let counter = 1;
 
 function addTask() {
   const target = document.getElementById("tasks");
-  const newElemenet = document.createElement("li");
-  newElemenet.textContent = "task " + counter;
-  newElemenet.addEventListener("click", removeTask);
-  target.appendChild(newElemenet);
+  const taskName = document.getElementById("taskName");
+  const newElement = document.createElement("li");
+
+  newElement.textContent = taskName.value;
+  newElement.addEventListener("dblclick", handleResolvedTask);
+
+  target.appendChild(newElement);
+
+  taskName.value = "";
   counter++;
 }
 
 function removeTask(e) {
   const tasks = document.getElementById("tasks");
   tasks.removeChild(e.target);
+}
+
+function handleResolvedTask(e) {
+  let currentDecoration = e.target.style.textDecoration;
+  if (currentDecoration === "line-through") {
+    e.target.style.textDecoration = "unset";
+  } else e.target.style.textDecoration = "line-through";
 }
