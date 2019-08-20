ffde2ddfa562bdadf40c30016972e6f658f894c5
Marcello Silva <marcellovcs@gmail.com>
2019-08-20 01:55:40 -0300

    fix comments and add redo icon

--- script.js
+++ script.js
@@ -3,50 +3,57 @@
 let items = [];
 
 function readArrayAndInsertNewItem() {
-  // saber onde colocar os itens na tela
+  // descobrindo onde colocar os itens na tela
   const target = document.getElementById("tasks");
   resetList();
 
-  // varrer o array
+  // varrendo o array
   items.forEach((item, idx) => {
-    // criar cada item
+    // criando cada item
     const newElement = document.createElement("li");
 
-    // atribuir o texto
+    // atribuindo o texto
     newElement.textContent = item.title;
-    // atribuir classe para diferenciar tarefas feitas das não feitas
+    // atribuindo classe para diferenciar tarefas feitas das não feitas
     newElement.classList = item.done ? "done" : "todo";
-    //atribuir id ao elemento
+    //atribuindo id ao elemento
     newElement.id = idx;
 
-    // criar container para colocar os icones
+    // criando container para colocar os icones
     const iconContainer = document.createElement("div");
+    // atribuindo class ao iconContainer
+    iconContainer.classList = "iconContainer";
 
-    // criar icone done
-    const doneIcon = document.createElement("img");
+    // criando icone handleStatusIcon
+    const handleStatusIcon = document.createElement("img");
     // atribuindo classe à tag img
-    doneIcon.classList = "icon doneIcon";
-    // adicionar evento de click para resolver tarefa
-    doneIcon.addEventListener("click", handleResolvedTask);
-    // criar icone clear
+    if (item.done) {
+      handleStatusIcon.classList = "icon redoIcon";
+    } else handleStatusIcon.classList = "icon doneIcon";
+
+    // adicionando evento de click para resolver tarefa
+    handleStatusIcon.addEventListener("click", handleResolvedTask);
+    // criando icone clear
     const clearIcon = document.createElement("img");
     // atribuindo classe à tag img
     clearIcon.classList = "icon clearIcon";
+    // adicionando evento de click para resolver tarefa
+    clearIcon.addEventListener("click", removeTask);
 
-    // adicionar os icones ao iconContainer
-    iconContainer.appendChild(doneIcon);
+    // adicionando os icones ao iconContainer
+    iconContainer.appendChild(handleStatusIcon);
     iconContainer.appendChild(clearIcon);
 
-    // adicionar iconContainer ao novo newElement
+    // adicionando iconContainer ao novo newElement
     newElement.appendChild(iconContainer);
 
-    // inserir cada item criado dentro do elemento html
+    // inserindo cada item criado dentro do elemento html
     target.appendChild(newElement);
   });
 }
 
 function addTaskIntoArray() {
-  // pegar o texto digitado
+  // pegando/selecionando o texto digitado
   const taskName = document.getElementById("taskName");
   // se não tiver nenhum valor no campo, a função chama o return e não faz nada
   if (taskName.value === "") return;
@@ -55,31 +62,43 @@ function addTaskIntoArray() {
     title: taskName.value,
     done: false
   });
-  // limpar campo de texto
+  // limpando campo de texto
   taskName.value = "";
   // chamando função para ler o array e montar a lista na tela
   readArrayAndInsertNewItem();
 }
 
-function handleResolvedTask(e) {
-  // debugger;
-  const id = e.currentTarget.parentElement.parentElement.id;
-  // identificar o elemento no array
+function handleResolvedTask(event) {
+  // pegando/selecionando item clicado a partir do ícone
+  //        / ícone clicado / elemento pai/elemento pai / id do elemento pai
+  const {id} = event.currentTarget.parentElement.parentElement;
+
+  // identificando o elemento no array
   const currentItem = items[id];
-  // alterar o done para o oposto do atual
+  // alterando o done para o oposto do atual
   currentItem.done = !currentItem.done;
-  // atribuir a mesma posição do array o item atual com a modificação
+  // atribuindo a mesma posição do array o item atual com a modificação
   items[id] = currentItem;
+
   // chamando função para ler o array e montar a lista na tela
   readArrayAndInsertNewItem();
 }
 
-function removeTask(e) {
-  const tasks = document.getElementById("tasks");
-  tasks.removeChild(e.target);
+function removeTask(event) {
+  // pegando/selecionando item clicado a partir do ícone
+  //        / ícone clicado / elemento pai/elemento pai / id do elemento pai
+  const {id} = event.currentTarget.parentElement.parentElement;
+
+  // removendo do array um item específico
+  items.splice(id, 1);
+
+  // chamando função para ler o array e montar a lista na tela
+  readArrayAndInsertNewItem();
 }
 
 function resetList() {
+  // identificando elemento no html cujo o id é "tasks"
   const target = document.getElementById("tasks");
+  // atribuindo ao elemento identificado um conteúdo em html igual à "" (nada/vazio)
   target.innerHTML = "";
 }
